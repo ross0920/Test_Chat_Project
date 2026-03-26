96fd67cb5908d6ac5bf9ad9e92f231955782dd6c
ross0920 <rosselaspoa@gmail.com>
2026-03-25 21:30:01 -0400

    c

--- Test_Chat_Server/Chat_Server.cpp
+++ Test_Chat_Server/Chat_Server.cpp
@@ -462,7 +462,7 @@ public:
 			ptr += sizeof(uint8_t);
 			size_t size = p.deserialize(ptr);
 			std::cout << "\tsize[" << size << "]\n";
-			ptr += len + 1;
+			ptr += size + 1;
 			it->second->deliver(msg_test_2);
 		}
 	}
