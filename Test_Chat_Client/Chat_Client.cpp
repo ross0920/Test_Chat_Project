39090ab01c464b9c04e86f8a3a3a677ec6f52ed7
ross0920 <rosselaspoa@gmail.com>
2026-04-26 13:00:11 -0400

    c

--- Test_Chat_Client/Chat_Client.cpp
+++ Test_Chat_Client/Chat_Client.cpp
@@ -569,12 +569,13 @@ private:
 			std::memcpy(m->body(), pOut, size);
 			auto buffer = boost::asio::buffer(m->data(), m->length());
 			auto self = shared_from_this();
-			std::cout << "send from ep port[" << udp_socket->local_endpoint().port() << "] ip[" << udp_socket->local_endpoint().address() << "]\n";
 
 			udp_socket->async_send_to(buffer, server_endpoint,
 				[this, self, size](boost::system::error_code ec, std::size_t bytes) {
 					//std::cout << "TRY SEND capture data to server\n";
 					if (!ec) {
+						std::cout << "send from ep port[" << udp_socket->local_endpoint().port() << "] ip[" << udp_socket->local_endpoint().address() << "]\n";
+
 						//std::cout << "send capture_ctx.ring_buffer data of size " << size << "\n";
 						ma_rb_commit_read(&capture_ctx.ring_buffer, size);
 						check_and_send_test();
